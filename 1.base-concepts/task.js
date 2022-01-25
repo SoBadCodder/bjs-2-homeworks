adaa2cd2c1699cea20e655ae1de4e5629ff853bf
SoBadCodder <deni-sim@mail.ru>
2022-01-25 22:56:36 +0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=c5acaccd393a6fd5ac21&state=ceb7d518-aa43-4217-8aca-601b862c2e70

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  const D = b ** 2 - 4 * a * c;
+  if (D == 0) {
+    arr.push(-b / (2 * a));
+  } else if (D > 0) {
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
+    arr.push((-b - Math.sqrt(D)) / (2 * a));
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  if (isNaN(Number(percent))) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+  if (isNaN(Number(contribution))) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  if (isNaN(Number(amount))) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  let P = percent / 12 / 100,
+    S = amount - contribution,
+    n = (date.getFullYear() - new Date().getFullYear()) * 12;
+  totalAmount = (S * (P + (P / (((1 + P) ** n) - 1))) * n).toFixed(2);
+  return parseFloat(totalAmount);
 }
