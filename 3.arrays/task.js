f0b93ae1d2760ce27c8d834e2cd96e9c86f53f18
SoBadCodder <deni-sim@mail.ru>
2022-01-28 14:39:43 +0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=c5acaccd393a6fd5ac21&state=ceb7d518-aa43-4217-8aca-601b862c2e70

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(item => item % 3 === 0 && item > 0).map(item => item * 10);
   return resultArr; // array
 }
