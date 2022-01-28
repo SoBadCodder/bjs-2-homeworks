fc736e2a46175708343bd4f114eb9efe0a5a328d
SoBadCodder <deni-sim@mail.ru>
2022-01-28 18:59:02 +0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=c5acaccd393a6fd5ac21&state=ceb7d518-aa43-4217-8aca-601b862c2e70

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,27 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
-
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) this.marks = [mark];
+  else this.marks.push(mark);
+}
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+}
+Student.prototype.getAverage = function () {
+  let result = this.marks.reduce(function (sum, current) {
+    return sum + current;
+  }) / this.marks.length;
+  return result;
+}
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
